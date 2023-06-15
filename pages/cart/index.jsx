f0b4cc14e0f6aac1923db071db9fa86bd764d187
Bima Indra <bimaindramulya@gmail.com>
2023-06-15 17:08:35 +0700

    add plus / minus item product

--- pages/cart/index.jsx
+++ pages/cart/index.jsx
@@ -1,7 +1,8 @@
 import Image from 'next/image';
 import { useDispatch, useSelector } from 'react-redux';
-import { removeProduct } from '../../store/feature/cart/cartSlice';
+import { removeProduct, addProductItem, reduceProductItem } from '../../store/feature/cart/cartSlice';
 import Link from 'next/link';
+import { useEffect } from 'react';
 
 const Cart = () => {
 	const cartItems = useSelector((state) => state.cart);
@@ -10,6 +11,29 @@ const Cart = () => {
 	const handleRemoveFromCart = (id) => {
 		dispatch(removeProduct(id));
 	};
+
+	const handleAddItem = (id) => {
+		dispatch(addProductItem(id));
+	};
+
+	const handleReduceItem = (id) => {
+		dispatch(reduceProductItem(id));
+	};
+
+	const handleSumPrice = () => {
+		let sumPrice = 0;
+		cartItems.length &&
+			cartItems.forEach((item) => {
+				const totalPriceProduct = item.price * item.count;
+				sumPrice += totalPriceProduct;
+			});
+
+		return sumPrice;
+	};
+
+	useEffect(() => {
+		handleSumPrice();
+	}, [cartItems]);
 
 	return (
 		<section>
@@ -37,31 +61,42 @@ const Cart = () => {
 												/>
 											</div>
 											<div className="min-w-0 flex-1">
-												<p className="mb-1 font-bold text-gray-900 line-clamp-2 dark:text-white lg:line-clamp-1">
+												<p className="mb-1 font-medium text-gray-900 line-clamp-2 dark:text-white lg:line-clamp-2">
 													{item.title}
 												</p>
 												<p className="truncate font-medium text-gray-500 dark:text-gray-400">${item.price}</p>
 											</div>
-											<div className="w-16 text-center">
-												<p className="mb-1">
-													<b>Item</b>
-												</p>
-												<span>1</span>
+											<div className="w-20 text-center">
+												<span>
+													<b>{item.count} pcs</b>
+												</span>
 											</div>
-											<button
-												className="rounded-lg bg-red-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-emerald-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
-												onClick={() => handleRemoveFromCart(item.id)}>
-												Remove
-											</button>
+											<div className="flex gap-x-2">
+												<button
+													className="rounded-lg bg-amber-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-amber-800 focus:outline-none focus:ring-4 focus:ring-emerald-300 dark:bg-amber-600 dark:hover:bg-amber-700 dark:focus:ring-amber-800"
+													onClick={() => handleAddItem(item.id)}>
+													+
+												</button>
+												<button
+													className="rounded-lg bg-amber-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-amber-800 focus:outline-none focus:ring-4 focus:ring-emerald-300 dark:bg-amber-600 dark:hover:bg-amber-700 dark:focus:ring-amber-800"
+													onClick={() => handleReduceItem(item.id)}>
+													-
+												</button>
+												<button
+													className="rounded-lg bg-red-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-emerald-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+													onClick={() => handleRemoveFromCart(item.id)}>
+													Remove
+												</button>
+											</div>
 										</div>
 									</li>
 								))
 							) : (
-								<div className="text-center">
-									<p className="mb-6">You haven't add anyting to the cart. </p>
-									<Link
-										className="inline-block rounded-lg bg-sky-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-sky-800 focus:outline-none focus:ring-4 focus:ring-emerald-300 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800"
-										href={`/`}>
+								<div className="prose mx-auto py-10 text-center">
+									<p className="mb-6">
+										Your cart is empty. <br /> You haven't add anyting to the cart.{' '}
+									</p>
+									<Link className="" href={`/`}>
 										Back to shop
 									</Link>
 								</div>
@@ -69,29 +104,31 @@ const Cart = () => {
 						</ul>
 					</div>
 				</div>
-				<div className="mx-auto my-6 w-full rounded-lg border border-gray-200 bg-white p-4 shadow dark:border-gray-700 dark:bg-gray-800 sm:p-8">
-					<div className="mb-4 flex items-center justify-between">
-						<h5 className="text-xl font-bold leading-none text-gray-900 underline dark:text-white">Summary</h5>
+				{cartItems.length > 0 && (
+					<div className="mx-auto my-6 w-full rounded-lg border border-gray-200 bg-white p-4 shadow dark:border-gray-700 dark:bg-gray-800 sm:p-8">
+						<div className="mb-4 flex items-center justify-between">
+							<h5 className="text-xl font-bold leading-none text-gray-900 underline dark:text-white">Summary</h5>
+						</div>
+						<div className="mt-6 flex justify-between">
+							<p className="font-semibold">Total price:</p>
+							<p className="text-2xl">
+								<b>$ {handleSumPrice()}</b>
+							</p>
+						</div>
+						<div className="mt-6 flex justify-end gap-4">
+							<Link
+								className="inline-block rounded-lg bg-sky-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-sky-800 focus:outline-none focus:ring-4 focus:ring-emerald-300 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800"
+								href={`/`}>
+								Back to shop
+							</Link>
+							<Link
+								className="inline-block rounded-lg bg-emerald-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-emerald-800 focus:outline-none focus:ring-4 focus:ring-emerald-300 dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800"
+								href={`/checkout`}>
+								Proceed to checkout
+							</Link>
+						</div>
 					</div>
-					<div className="mt-6 flex justify-between">
-						<p className="font-semibold">Total Payment:</p>
-						<p className="text-2xl">
-							<b>$ 200</b>
-						</p>
-					</div>
-					<div className="mt-6 flex justify-end gap-4">
-						<Link
-							className="inline-block rounded-lg bg-sky-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-sky-800 focus:outline-none focus:ring-4 focus:ring-emerald-300 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800"
-							href={`/`}>
-							Back to shop
-						</Link>
-						<Link
-							className="inline-block rounded-lg bg-emerald-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-emerald-800 focus:outline-none focus:ring-4 focus:ring-emerald-300 dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800"
-							href={`/`}>
-							Proceed to checkout
-						</Link>
-					</div>
-				</div>
+				)}
 			</div>
 		</section>
 	);
